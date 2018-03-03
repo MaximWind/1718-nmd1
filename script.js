bc89836daff571fc51669825c7095b8b43073371
MaximWind <maxiwind@student.arteveldehs.be>
2018-03-04 00:38:59 +0100

    update cv 2

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 //when button is pressed, navigation is shown
 $('.fa-bars').click(function() {
-    $('.main_navigation').slideToggle(1000);
-})
-
-//arrow and stripe of list item work
-$('#list_item4').mouseenter(function() {
-    $('#far_down').addClass('fa-minus-square')
-})
-
-$('#list_item4').mouseleave(function() {
-    $('#far_down').removeClass('fa-minus-square')
+    $('.navbar').slideToggle(500);
 })
