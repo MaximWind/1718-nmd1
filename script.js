49b21e84d4b58bfd1c95c592758b812975a2ce54
MaximWind <maxiwind@student.arteveldehs.be>
2018-03-05 20:24:03 +0100

    update nav official

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+//when document load => page is at the top
+$(document).ready(function() {
+    $(this).scrollTop(0);
+});
+
 //when button is pressed, navigation is shown
 $('.fa-bars').click(function() {
     $('.navbar').slideToggle(500);
+})
+
+
+//when clicked on navigation => cards are flipped
+$('#buttonCard1').click(function() {
+    $('#card1').css('transform', 'rotateY(' + 180 + 'deg)');
+    console.log('card1 flipped')
+})
+$('#buttonCard2').click(function() {
+    $('#card2').css('transform', 'rotateY(' + 180 + 'deg)');
+    console.log('card2 flipped')
+})
+$('#buttonCard3').click(function() {
+    $('#card3').css('transform', 'rotateY(' + 180 + 'deg)');
+    console.log('card3 flipped')
 })
